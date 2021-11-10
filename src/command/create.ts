db48f70bb77351f7e388340df223fcefacbb16f1
hejinzhi <504151911@qq.com>
2021-11-10 15:18:43 +0800

    增加 loading

--- src/command/create.ts
+++ src/command/create.ts
@@ -3,12 +3,11 @@ import {
   downloadTemplate,
   selectFeature,
 } from '../utils/create';
-// import ora from 'ora';
-
+import * as ora from 'ora';
 export async function create(name: string) {
   const feature = await selectFeature();
-  // const spinner = ora('正在下载...').start();
+  const spinner = ora('正在下载...').start();
   await downloadTemplate(feature.url, name);
-  // spinner.stop();
+  spinner.stop();
   changePkgName(`${name}/package.json`, name, feature.type);
 }
